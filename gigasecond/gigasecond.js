4390a2bd29649b85ba43fcf46d2131ba0923336a
kassim Damilola <eidetic@outlook.com>
2019-08-06 17:10:51 +0100

    giggasecond solution

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -3,6 +3,7 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+export const gigasecond = (givenMoment) =>  new Date(givenMoment.getTime() + GigaSecond * 1000)
+
+
+const GigaSecond = 1000000000
