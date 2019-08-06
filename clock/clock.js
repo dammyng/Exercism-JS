5e8352cbe4b5fb177bf884a22e4bcc404b07dc3f
kassim Damilola <eidetic@outlook.com>
2019-08-06 19:43:47 +0100

    Clock Task Passed

--- clock/clock.js
+++ clock/clock.js
@@ -4,23 +4,36 @@
 //
 
 export class Clock {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(hour, minutes =0) {
+    this.minutes = ((minutes % 60) + 60) % 60
+    this.hour = (((hour + (this.normalizeMinutes(minutes))) % 24)  + 24)  % 24
   }
 
   toString() {
-    throw new Error('Remove this statement and implement this function');
+    return  this.padZero(this.hour) + ":" +  this.padZero(this.minutes)
   }
 
-  plus() {
-    throw new Error('Remove this statement and implement this function');
+  plus(value) {
+    return new Clock(this.hour, this.minutes + value);
   }
 
-  minus() {
-    throw new Error('Remove this statement and implement this function');
+  minus(value) {
+    return this.plus(-value);
   }
 
-  equals() {
-    throw new Error('Remove this statement and implement this function');
+  equals(value) {
+    return this.toString() === value.toString();
+  }
+
+  padZero(time){
+    return time.toString().length == 1? `0${time}` : time
+  }
+
+  normalizeMinutes(minutes){
+    return  (minutes -  this.minutes % 60) / 60
+  }
+
+  normalizeHour (){
+    
   }
 }
