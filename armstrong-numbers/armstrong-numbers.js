542b9e3e3d1e753c5120a0bdbe4996e556e9c626
kassim Damilola <eidetic@outlook.com>
2019-08-18 20:41:26 +0100

    armstrong numbers correction

--- armstrong-numbers/armstrong-numbers.js
+++ armstrong-numbers/armstrong-numbers.js
@@ -3,18 +3,18 @@
 // convenience to get you started writing code faster.
 //
 
-export const validate = (value) => {
-  let vArray = [...String(value)]
-  let sum = 0
+export const validate = value => {
+  let vArray = [...String(value)];
+  let sum = 0;
 
-  vArray.map((_, index) =>
-    sum += getDigit(value, index) ** vArray.length
-  )
-  return sum == value
+  vArray.forEach((_, index) => {
+    sum += getDigit(value, index) ** vArray.length;
+  });
+
+  return sum == value;
 };
 
-
 let getDigit = (number, n) => {
-  let digit = parseInt(String(number).charAt(n))
-  return digit
-}
+  let digit = parseInt(String(number).charAt(n));
+  return digit;
+};
