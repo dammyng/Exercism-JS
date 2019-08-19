873c6d4a47933a2cc9bac1862c75627b7c9514d5
kassim Damilola <eidetic@outlook.com>
2019-08-19 05:49:16 +0100

    rna-transcription - convention corrections

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,13 +1,9 @@
-//
-// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 
-export const toRna = (DNA) =>
-  DNA.split('').map(DnaChar => DnaRnaTable[DnaChar]).join('')
+export const toRna = (Dna) =>
+Dna.split('').map(nucleotide => transcriptionMap[nucleotide]).join('')
 
 
-let DnaRnaTable = {
+let transcriptionMap = {
 'G': 'C',
 'C':'G',
 'T':'A',
