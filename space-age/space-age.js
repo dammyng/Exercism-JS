aaa38844bd51f23c808b1761eaacfd853c6ffc7c
kassim Damilola <eidetic@outlook.com>
2019-08-20 18:53:26 +0100

    space age correction

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -1,16 +1,16 @@
-export const age = (planet, ageInSeconds) => roundToTwo(ageInSeconds / PLANET_REV_MAP[planet])
+export const age = (planet, ageInSeconds) => roundToTwo( ageInSeconds / (EARTH_ORBITAL_PERIOD * ORBITAL_PERIOD_RELATIVE_TO_EARTH[planet]))
 
 let roundToTwo = num => (Math.round(num * 1e2) / 1e2);
 
-const EARTH_REV = 31557600
+const EARTH_ORBITAL_PERIOD = 31557600
 
-const PLANET_REV_MAP = {
-  "earth": EARTH_REV,
-  "mercury": EARTH_REV * 0.2408467,
-  "venus": EARTH_REV * 0.61519726,
-  "mars": EARTH_REV * 1.8808158,
-  "jupiter": EARTH_REV * 11.862615,
-  "saturn": EARTH_REV * 29.447498,
-  "uranus": EARTH_REV * 84.016846,
-  "neptune": EARTH_REV * 164.79132,
+const ORBITAL_PERIOD_RELATIVE_TO_EARTH = {
+  "earth": 1,
+  "mercury": 0.2408467,
+  "venus": 0.61519726,
+  "mars": 1.8808158,
+  "jupiter": 11.862615,
+  "saturn": 29.447498,
+  "uranus": 84.016846,
+  "neptune": 164.79132,
 }
