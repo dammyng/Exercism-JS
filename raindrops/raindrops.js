844e64ec00f1e892d6890075cbfea51d091b7515
kassim Damilola <eidetic@outlook.com>
2019-08-27 15:20:42 +0100

    raindrop solution

--- raindrops/raindrops.js
+++ raindrops/raindrops.js
@@ -3,6 +3,18 @@
 // convenience to get you started writing code faster.
 //
 
-export const convert = () => {
-  throw new Error("Remove this statement and implement this function");
+export const convert = (value) => {
+  let result = ""
+  factors.forEach(element => {
+    if(value % element == 0)
+      result += RainStringMap[element]
+  });
+  return result == "" ? value.toString() : result
 };
+
+let factors = [3,5,7]
+let rainStingMap = {
+3:"Pling",
+5:"Plang",
+7:"Plong"
+}
