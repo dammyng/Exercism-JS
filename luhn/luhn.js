fb94d3ba1a1fedf9357590d47fa3840f7c0c124d
kassim Damilola <eidetic@outlook.com>
2019-08-27 14:59:52 +0100

    test passed.. not submitted - bad code

--- luhn/luhn.js
+++ luhn/luhn.js
@@ -4,11 +4,35 @@
 //
 
 export class Luhn {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(value) {
+    this.valueArray = [...value.split(' ').join('')].map(i=>Number(i)).reverse()
+
+
   }
 
   get valid() {
-    throw new Error("Remove this statement and implement this function");
+  
+    if(this.valueArray.length <2)
+      return false
+    
+    const REG = /^\d+$/;
+      if (REG.test(this.valueArray.join()))
+       return false
+
+   let sum =  doubles(this.valueArray).reduce(function(accumulator, currentValue, currentIndex, array) {
+      return accumulator + currentValue;
+    });
+
+   return sum % 10 ==0
   }
+}
+
+let doubles = (valueArray)=>{
+  return valueArray.map((v,i)=>{
+    if (i %2 ==1){
+       let a = v *2 
+       return a >9? a-9 :a
+    }
+    return v
+  })
 }
